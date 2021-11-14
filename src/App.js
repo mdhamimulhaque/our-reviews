deb0cc01552f788ff4517ffb6ba4df365a2ef405
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-11-14 15:17:11 +0600

    Our Reviews React Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import SliderData from "./data";
+import { FaAngleRight,FaAngleLeft } from 'react-icons/fa';
+import { useState } from 'react';
+
 
 function App() {
+  const [ index,setIndex ] = useState(0);
+  const { name,job,image,text } = SliderData[index];
+
+
+//---------- next --------
+  const nextSlider = () =>{
+    setIndex((index)=>{
+      const newIndex = index + 1;
+      return newIndexNumber(newIndex);
+    })
+  }
+
+  //----------  previous --------
+  const prevSlider = () =>{
+    setIndex((index)=>{
+      const newIndex = index - 1;
+      return newIndexNumber(newIndex);
+    })
+  }
+
+    //----- manage slider -----------
+    const newIndexNumber = (num) =>{
+      if(num > SliderData.length - 1){
+          return 0;
+      }
+      if(num < 0){
+          return SliderData.length - 1;
+      }
+      return num; 
+  }
+
+  //------- random slider ----
+  const randomSlider = () =>{
+    let randomIndex = Math.floor(Math.random() * SliderData.length);
+    //---avoid same number ----
+    if(randomIndex === index){
+      randomIndex = index + 1;
+    };
+    setIndex(newIndexNumber(randomIndex));
+  };
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h2>Our Reviews</h2>
+            <div className="review-box">
+                <img src={image} alt="" />
+                <div className="info-box">
+                    <h4>{name}</h4>
+                    <h6>{job}</h6>
+                    <p>{text}</p>
+                </div>
+                <div className="icon-box">
+                    <span onClick={prevSlider}><FaAngleLeft /></span>
+                    <span onClick={nextSlider}><FaAngleRight /></span>
+                </div>
+                <button onClick={randomSlider}>Random Slider</button>
+            </div>
+        </main>
   );
 }
 
